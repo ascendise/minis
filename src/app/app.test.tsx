42e7872ee3d11e5dfe16de19fca6df5633b8bb50
ascendise <60201207+ascendise@users.noreply.github.com>
2022-12-06 20:45:08 +0100

    ci: run linter and prettier on pull requests (#2)
    * added ESLint
    
    * add prettier
    
    * run prettier
    
    * workflow for checking code for violations of code rules

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -1,17 +1,18 @@
 import { cleanup, render, screen } from '@testing-library/react';
 import App from './app';
+import React from 'react';
 
 afterEach(cleanup);
 
 it('should render website name', () => {
-    render(<App/>);
-    const title = screen.queryByText('Minis!')
-    expect(title).toBeInTheDocument();
-})
+  render(<App />);
+  const title = screen.queryByText('Minis!');
+  expect(title).toBeInTheDocument();
+});
 
 it('should render logo on home page', () => {
-    render(<App/>);
-    const logo = screen.queryByRole('img');
-    expect(logo).toBeInTheDocument();
-    expect(logo?.getAttribute('src')).toBe('./logo.svg');
-})
+  render(<App />);
+  const logo = screen.queryByRole('img');
+  expect(logo).toBeInTheDocument();
+  expect(logo?.getAttribute('src')).toBe('./logo.svg');
+});
