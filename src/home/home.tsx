16c1dca5f34cc3a3db1dc12595d976d42dd0205d
Ascendise <rapperido@gmail.com>
2022-12-06 17:38:31 +0100

    banner for home page

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -3,8 +3,12 @@ import React from 'react';
 export default function Home() {
     return(
         <div>
-            <div>Minis!</div>
-            <img src='./logo.svg' alt='paint brush with brown handle and purple paint'></img>
+            <div className='text-center bg-orange-300 rounded-b-xl'>
+                <h1 className='text-white text-6xl font-bold inline'>
+                    Minis!
+                    <img className='inline h-10' src='./logo.svg' alt='paint brush with brown handle and purple paint'></img>
+                </h1>
+            </div>
         </div>
     )
 }
