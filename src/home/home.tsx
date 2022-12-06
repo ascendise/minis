130272de5c43641df6c795abb3928e041aa17966
Ascendise <rapperido@gmail.com>
2022-12-06 17:00:03 +0100

    alt text for image

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return(
         <div>
             <div>Minis!</div>
-            <img src='./logo.svg'></img>
+            <img src='./logo.svg' alt='paint brush with brown handle and purple paint'></img>
         </div>
     )
 }
