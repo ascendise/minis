f900b3122f42bc492c3e16dae1bf9b6751e9b561
Ascendise <rapperido@gmail.com>
2022-12-14 01:08:19 +0100

    better coloring

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,8 +4,8 @@ import Home from '../home/home';
 
 export default function App() {
   return (
-    <div>
-      <div className="text-center bg-orange-300 rounded-b-xl">
+    <div className='flex flex-col h-screen bg-orange-200'>
+      <div className="text-center bg-orange-900 rounded-b-xl">
         <h1 className="text-white text-6xl font-bold inline">
           Minis!
           <img className="inline h-10" src="./logo.svg" alt="paint brush with brown handle and purple paint"></img>
