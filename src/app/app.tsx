1024eb4eb22aba4bcc68d0ff8342ebceb2133d8a
Ascendise <rapperido@gmail.com>
2022-12-14 12:35:16 +0100

    better colour for banner to not drown out logo

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -5,7 +5,7 @@ import Home from '../home/home';
 export default function App() {
   return (
     <div className='flex flex-col h-screen bg-orange-200'>
-      <div className="text-center bg-orange-900 rounded-b-xl">
+      <div className="text-center bg-orange-700 rounded-b-xl">
         <h1 className="text-white text-6xl font-bold inline">
           Minis!
           <img className="inline h-10" src="./logo.svg" alt="paint brush with brown handle and purple paint"></img>
